566b94a8624de4603af5cd259cce6a10d3538bd9
ManashX <manash20_ug@cse.nits.ac.in>
2023-05-24 12:18:08 +0530

    storing in local db using api server

--- books/src/App.js
+++ books/src/App.js
@@ -1,33 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BookCreate from './components/BookCreate';
 import BookList from "./components/BookList";
+import axios from 'axios';
 
 
 function App(){
     const [books, setBooks] = useState([]);
-    const deleteBookById = (id)=>{
+
+    const fetchBooks = async () =>{
+        const response = await axios.get('http://127.0.0.1:3001/books');
+        setBooks(response.data);
+    };
+    useEffect(() =>{
+        fetchBooks();
+    },[]);
+
+    const deleteBookById = async (id)=>{
+        axios.delete(`http://127.0.0.1:3001/books/${id}`);
         const removeBook = books.filter((book)=>{
             return book.id !== id;
         });
         setBooks(removeBook);
     };
-    const handleSubmit = (bookName)=>{
-        const addBook = [...books,{id:Math.round(Math.random() * 9999), title: bookName}];
+    const createBook = async (bookName)=>{
+        const response = await axios.post('http://127.0.0.1:3001/books',{
+            title: bookName
+        });
+        const addBook = [...books,
+            // {id:Math.round(Math.random() * 9999), title: bookName}
+            response.data
+        ];
         setBooks(addBook);
     };
-    const updateBookById = (id, newTitle) =>{
+    const updateBookById = async(id, newTitle) =>{
+        const response = await axios.put( `http://127.0.0.1:3001/books/${id}`,{
+            title : newTitle
+        });
+        //one of the most critical feature...go through the video
+        
         const updatedBooks = books.map((book) =>{
             if(book.id === id){
-                return {...book,title: newTitle};
+                return {...book,...response.data};
+                //response.data was the updated book object that came back from the API.
+                //... means take all the different properties out of that object, take all the different key value and add them into this new object right here.
             }
             return book;
         });
         setBooks(updatedBooks);
     };
+    
     return (<div className="app">
         <h1>Reading List</h1>
         <BookList books = {books} onDelete = {deleteBookById} onEdit = {updateBookById}/>
-        <BookCreate onSubmit = {handleSubmit}/>
+        <BookCreate onSubmit = {createBook}/>
     </div>)
 }
 export default App;
